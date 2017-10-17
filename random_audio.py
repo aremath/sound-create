d8bbef6efbabe04d760d464aba8d09da1f47bc1d
Ross <rmawhorter@gmail.com>
2017-10-17 16:11:07 -0700

    added more complex position constraints to random_audio

--- random_audio.py
+++ random_audio.py
@@ -3,28 +3,65 @@ import classes
 import os
 import sys
 
-def random_sounds(sound_files, n, start_times, positions):
-    """make some random sounds in the specified ranges from the file list"""
-    sound_objects = []
-    for i in range(n):
-        sound_file = random.choice(sound_files)
-        start_time = random.uniform(start_times[0], start_times[1])
-        pos  = []
-        # get a random value for every dimension of position specified
-        for position_range in positions:
-            pos.append(random.uniform(position_range[0], position_range[1]))
-        assert len(positions) == 3, "Position should have three dimensions"
-        so = classes.SoundObject(str(i), tuple(pos), sound_file, start_time)
-        sound_objects.append(so)
-    return sound_objects
+class PositionDistribution:
 
-# given a path to a directory, make some random sounds and listen to them
+    def __init__(self, distribution):
+        """distribution is a list of lists of intervals. Each element describes a rectangular prism."""
+        self.distribution = distribution
+
+    def choose_position(self):
+        """choose a position uniformly out of the entire set of rectangles"""
+        # weight each prism by volume
+        weighted_prisms = [(prism, find_volume(prism)) for prism in self.distribution]
+        # choose one uniformly
+        choice = weighted_choice(weighted_prisms)
+        # choose a position within that prism uniformly
+        position = [random.uniform(*interval) for interval in choice]
+        return tuple(position)
+
+def find_volume(intervals):
+    """find the volume of a 3-dimensional rectangular prism"""
+    assert len(intervals) == 3
+    breadth = intervals[0][1] - intervals[0][0]
+    width = intervals[1][1] - intervals[1][0]
+    height = intervals[2][1] - intervals[2][0]
+    return abs(breadth * width * height)
+
+def weighted_choice(choices):
+    """returns an item chosen at random from choices; expects choices to be a list of tuples of (choice, weight)"""
+    total_weight = sum([weight for choice, weight in choices])
+    # choose a number to pick a choice
+    r = random.uniform(0, total_weight)
+    accumulate = 0
+    for choice, weight in choices:
+        accumulate += weight
+        if accumulate > r:
+            return choice
+    assert False, "weighted choice somehow broken"
+        
+def random_sounds(sound_files, distribution, nsounds, start_times):
+    sobjects = []
+    for i in range(nsounds):
+        sound_file = random.choice(sound_files)
+        start_time = random.uniform(*start_times)
+        pos = distribution.choose_position()
+        assert len(pos) == 3, "Position should have 3 dimensions"
+        so = classes.SoundObject(str(i), pos, sound_file, start_time)
+        sobjects.append(so)
+    return sobjects
+
 def random_data(mic_array, path_to_sounds, nsounds, start_times, position_limits):
+    """given a path to a directory, make some random sounds and listen to them"""
     # use sound files at the top level with .wav extension
     sound_files = os.listdir(path_to_sounds)
     sound_files = filter(lambda x: x.split(".")[-1] == "wav", sound_files)
     # give the direct path to the file (rather than just the name)
     sound_files = map(lambda x: path_to_sounds + "/" + x, sound_files)
-    sounds = random_sounds(sound_files, nsounds, start_times, position_limits)
+    sounds = random_sounds(sound_files, position_limits, nsounds, start_times)
     classes.hear_all(sounds, mic_array)
 
+def random_data_crowd(mic_array, normal_sounds, normal_dist, nnormal, crowd_sounds, crowd_dist, ncrowd, start_times):
+    normal_sobjects = random_sounds(normal_sounds, normal_dist, nnormal, start_times)
+    crowd_sobjects = random_sounds(crowd_sounds, crowd_dist, ncrowd, start_times)
+    classes.hear_all(normal_sobjects, mic_array)
+    classes.hear_all(crowd_sobjects, mic_array)
--- test.py
+++ test.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     sl1 = classes.SoundListener("sl1", (0, 0, 0), (1, 0, 0), polar_pattern_=lambda x: math.cos(x))
     sl2 = classes.SoundListener("sl2", (10, 0, 4), (0, 1, 0))
     listeners = [sl1, sl2]
-    random_audio.random_data(listeners, "..", 10, (0, 10), [(0, 10), (0, 10), (0, 10)])
+    distribution = random_audio.PositionDistribution([[(0, 10), (0, 10), (0, 10)]])
+    random_audio.random_data(listeners, "..", 10, (0, 10), distribution)
     for l in listeners:
         l.write_sound()
